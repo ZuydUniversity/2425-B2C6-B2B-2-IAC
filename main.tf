8c40abd84293be627c276eb82394d0e271d1b6e3
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-05-21 11:45:21 +0200

    whoopsie hihi

--- main.tf
+++ main.tf
@@ -5,10 +5,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
-
-    backend "azurerm" {}
   }
 
+  backend "azurerm" {}
+  
   required_version = ">= 1.1.0"
 }
 
