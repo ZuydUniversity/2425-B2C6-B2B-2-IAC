9908203b26475f8e5afda4764b179374e3ea0154
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-06-07 16:20:44 +0200

    Added delegation block to subnets

--- main.tf
+++ main.tf
@@ -57,6 +57,18 @@ resource "azurerm_subnet" "frontend" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "aci-delegation"
+
+    service_delegation {
+      name = "Microsoft.ContainerInstance/containerGroups"
+
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action",
+      ]
+    }
+  }
 }
 
 # Create virtual network for backend
@@ -65,6 +77,18 @@ resource "azurerm_subnet" "backend" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "aci-delegation"
+
+    service_delegation {
+      name = "Microsoft.ContainerInstance/containerGroups"
+
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action",
+      ]
+    }
+  }
 }
 
 # Create a network profile for connecting endpoints to the subnet
