ee7a7c1398f4923e75b36eb0cb27b866abb79d98
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-06-07 15:41:35 +0200

    First attempt at creating a virutal network

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
 
-  backend "azurerm" {}
+  #backend "azurerm" {}
 
   required_version = ">= 1.1.0"
 }
@@ -26,11 +26,40 @@ variable "image_registry_password" {
   sensitive = true
 }
 
-
 # Define the resource group
 resource "azurerm_resource_group" "rg" {
   name     = "2425-B2C6-B2B-2"
   location = "westeurope"
+}
+
+# Create a security group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "network-security-group"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "virtual-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "frontend-subnet"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "backend-subnet"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.nsg.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
 
 # Create the container registry
